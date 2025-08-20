74e2c3d37268e63ab2dc0ff5f9b4732cee9850b2
Fatima <fatimachughtai@cmail.carleton.com>
2025-08-20 18:25:00 -0400

    srever updated

--- six-months/server.js
+++ six-months/server.js
@@ -6,11 +6,11 @@ const app = express();
 app.use(cors());
 
 
-const directoryPathVancouver = path.join(__dirname, 'public/trip-pictures/vancouver');
-const directoryPathOttawa = path.join(__dirname, 'public/trip-pictures/ottawa');
-const directoryPathMontreal = path.join(__dirname, 'public/trip-pictures/montreal');
-const directoryPathDowntownNYC = path.join(__dirname, 'public/trip-pictures/NYC-Downtown');
-const directoryPathTarrytownNYC = path.join(__dirname, 'public/trip-pictures/NYC-Tarrytown');
+const directoryPathVancouver = path.join(__dirname, 'client/public/trip-pictures/vancouver');
+const directoryPathOttawa = path.join(__dirname, 'client/public/trip-pictures/ottawa');
+const directoryPathMontreal = path.join(__dirname, 'client/public/trip-pictures/montreal');
+const directoryPathDowntownNYC = path.join(__dirname, 'client/public/trip-pictures/NYC-Downtown');
+const directoryPathTarrytownNYC = path.join(__dirname, 'client/public/trip-pictures/NYC-Tarrytown');
 
 
 app.get('/api/images/vancouver', (req, res) => {
@@ -58,10 +58,20 @@ app.get('/api/images/tarrytownNYC', (req, res) => {
   });
 });
 
-app.use('/trip-images/vancouver', express.static(path.join(__dirname, 'public/trip-pictures/vancouver')));
-app.use('/trip-images/ottawa', express.static(path.join(__dirname, 'public/trip-pictures/ottawa')));
-app.use('/trip-images/montreal', express.static(path.join(__dirname, 'public/trip-pictures/montreal')));
-app.use('/trip-images/downtownNYC', express.static(path.join(__dirname, 'public/trip-pictures/NYC-Downtown')));
-app.use('/trip-images/tarrytownNYC', express.static(path.join(__dirname, 'public/trip-pictures/NYC-Tarrytown')));
+app.use('/trip-images/vancouver', express.static(path.join(__dirname, 'client/public/trip-pictures/vancouver')));
+app.use('/trip-images/ottawa', express.static(path.join(__dirname, 'client/public/trip-pictures/ottawa')));
+app.use('/trip-images/montreal', express.static(path.join(__dirname, 'client/public/trip-pictures/montreal')));
+app.use('/trip-images/downtownNYC', express.static(path.join(__dirname, 'client/public/trip-pictures/NYC-Downtown')));
+app.use('/trip-images/tarrytownNYC', express.static(path.join(__dirname, 'client/public/trip-pictures/NYC-Tarrytown')));
 
-app.listen(5000, () => console.log('Server running on http://localhost:5000'));
+// ---------- ADD THIS PART FOR REACT FRONTEND ----------
+const clientBuildPath = path.join(__dirname, 'client/build');
+app.use(express.static(clientBuildPath));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(clientBuildPath, 'index.html'));
+});
+// ------------------------------------------------------
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
